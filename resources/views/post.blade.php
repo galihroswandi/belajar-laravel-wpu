d508851aada541feb13ddd3fb525e847d2eaa0b8
galihroswandi <galihroswandi25@gmail.com>
2023-09-08 15:22:54 +0700

    feat: Database Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,8 @@
 @section('container')
     <article class="mb-5">
         <h1>{{ $post->title }}</h1>
-        <p>By: Galih Roswandi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By: <a href="#">{{ $post->user->name }}</a> in <a
+                href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
         <a href="/blog">&laquo; Kembali ke posts</a>
     </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,6 +7,8 @@
             <a href="/post/{{ $post->slug }}">
                 <h1 class="fs-4">{{ $post->title }}</h1>
             </a>
+            <p class="mt-3">By: <a href="#">{{ $post->user->name }}</a> in <a
+                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
