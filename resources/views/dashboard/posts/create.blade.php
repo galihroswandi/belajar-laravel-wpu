e93b7bb714062a775829ba723b79cc95fa3bf3d0
galihroswandi <galihroswandi25@gmail.com>
2023-09-23 11:28:03 +0700

    feat: Validation & Insert Post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -6,27 +6,52 @@
     </div>
 
     <div class="col-md-7">
-        <form method="POST" action="/dashboard/posts">
+        <form method="POST" action="/dashboard/posts" class="mb-4">
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" autocomplete="off">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                    autocomplete="off" value="{{ old('title') }}" autofocus required>
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="slug" class="form-label">Slug</label>
-                <input type="text" class="form-control" name="slug" id="slug" autocomplete="off">
+                <input type="text" class="form-control @error('slug') is-invalid @enderror" name="slug" id="slug"
+                    autocomplete="off" value="{{ old('slug') }}" required>
+                @error('slug')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="category" class="form-label">Category</label>
-                <select class="form-select" name="category" id="category">
+                <label for="category_id" class="form-label">Category</label>
+                <select class="form-select @error('category_id') is-invalid @enderror" name="category_id" id="category_id"
+                    required>
                     @foreach ($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @if (old('category_id') == $category->id)
+                            <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                        @else
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endif
                     @endforeach
                 </select>
+                @error('category_id')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
-                <input id="body" type="hidden" name="body">
+                @error('body')
+                    <span class="text-danger d-block">{{ $message }}</span>
+                @enderror
+                <input id="body" type="hidden" name="body" value="{{ old('body') }}" required>
                 <trix-editor input="body"></trix-editor>
             </div>
             <button type="submit" class="btn btn-primary">Create new post</button>
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,6 +5,25 @@
         <h1 class="h2">My Posts</h1>
     </div>
 
+    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+        <symbol id="check-circle-fill" viewBox="0 0 16 16">
+            <path
+                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+        </symbol>
+    </svg>
+
+    @if (session('success'))
+        <div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:">
+                <use xlink:href="#check-circle-fill" />
+            </svg>
+            <div>
+                {{ session('success') }}
+            </div>
+        </div>
+    @endif
+
+
     <div class="table-responsive small col-lg-8">
         <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
         <table class="table table-striped table-sm">
